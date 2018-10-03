f328e66f1ba211ab95e2a35005257abcbb4a9147
chaiwat wattayakorn <chairedevil@live.com>
2018-10-03 18:23:33 +0900

    first commit

---
+++ botman.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('vendor/autoload.php');
+require_once('lib/replyMsg.class.php');
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+
+$config = [
+
+];
+
+// Load the driver(s) you want to use
+DriverManager::loadDriver(\BotMan\Drivers\Web\WebDriver::class);
+
+// Create an instance
+$botman = BotManFactory::create($config);
+
+$botman->hears('#{word}', function ($bot, $word) {
+
+    $rm = new replyMsg();
+    $replyResult = $rm->translate($word);
+
+    $bot->reply($replyResult, [
+        'type' => 'translate',
+    ]);
+
+});
+
+$botman->hears('@{userId}', function ($bot, $userId) {
+
+    $rm = new replyMsg();
+    $instaData = $rm->getLastInsta($userId);
+
+    $bot->reply($replyResult, [
+        'type' => 'instagram',
+        'imgSrc' => $instaData['imgSrc']
+    ]);
+
+});
+
+$botman->hears('hello', function (BotMan $bot) {
+    $bot->reply('Hello yourself.');
+});
+
+// Start listening
+$botman->listen();
---
+++ lib/replyMsg.class.php
@@ -0,0 +1,57 @@
+<?php
+    require_once ('./vendor/autoload.php');
+    use \Dejurin\GoogleTranslateForFree;
+    use \DetectLanguage\DetectLanguage;
+
+    class replyMsg
+    {
+        public function translate($msg){
+
+            $tr = new GoogleTranslateForFree();
+
+            DetectLanguage::setApiKey("87bd89dad3e829243777382a605b2dc2");
+            $languageCode = DetectLanguage::simpleDetect($msg);
+
+            if($languageCode == 'ja'){
+                $returnMsg = $tr->translate('ja', 'en', $msg);
+            }else{
+                $returnMsg = $tr->translate('auto', 'ja', $msg);
+            }
+
+            //$returnMsg = $tr->translate('ja', 'en', $msg);
+
+            //if($msg == $returnMsg){
+            //    $returnMsg = $tr->translate('en', 'ja', $msg);
+            //}
+
+            return $returnMsg;
+        }
+
+        public function getLastInsta($userId){
+
+            $cache = new Instagram\Storage\CacheManager('instragram_cache');
+
+            $api = new Instagram\Api($cache);
+            $api->setUserName($userId);
+
+            try {
+
+                $feed = $api->getFeed();
+                $thumSrc = $feed->getMedias()[0]->getThumbnailSrc();
+                $imgSrc = $feed->getMedias()[0]->getDisplaySrc();
+                $img = array();
+                $img['thumSrc'] = $thumSrc;
+                $img['imgSrc'] = $imgSrc;
+
+            } catch (Exception $exception) {
+                print_r($exception->getMessage());
+            } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+                print_r($exception->getMessage());
+            }
+
+
+            return $img;
+        }
+    }
+
+?>
