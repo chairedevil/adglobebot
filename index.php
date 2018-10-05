44b63ae490a0c7fc35cb510011c50b8fce5597be
Khanit <omahha7@gmail.com>
2018-10-05 17:35:31 +0900

    t

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     if(isset($_SESSION["ses_login_accToken_val"])){
         $accToken = $_SESSION["ses_login_accToken_val"];
 
-        $userInfo = $LineLogin->userProfile($accToken,true);
+        //$userInfo = $LineLogin->userProfile($accToken,true);
         /*if(!is_null($userInfo) && is_array($userInfo) && array_key_exists('userId',$userInfo)){
             echo "<pre>";
             print_r($userInfo);
