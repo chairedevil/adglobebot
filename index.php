d5efd66076e16d25e80e1b12f5341c284c0b3f32
Khanit <omahha7@gmail.com>
2018-10-09 11:38:16 +0900

    t

--- index.php
+++ index.php
@@ -339,7 +339,7 @@
                                 $botText = $botText+'<div class="card" style="max-width: 300px">';
                                 $botText = $botText+'<div class="card-header msgHeader">Bot</div>';
                                 $botText = $botText+'<div class="card-body p-2">';
-                                $botText = $botText+'<p class="card-text msgBody">'+　result.messages[0].additionalParameters.errMsg　+'</p>';
+                                $botText = $botText+'<p class="card-text msgBody">error: '+　result.messages[0].additionalParameters.errMsg　+'</p>';
                                 $botText = $botText+'</div>';
                                 $botText = $botText+'<div class="card-footer text-muted text-right bg-white msgTime">'+ $replyTime +'</div>';
                                 $botText = $botText+'</div>';
